610543ee676bb4320dcc783b60e318922dd23775
arjun08soni <arjun08soni@gmail.com>
2020-09-06 19:02:08 +0530

    Updates

---
+++ precache-manifest.f43ea5c2e785d23417a688930f704117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2779ba94ab31bb2b5d125f9e3855ec0",
+    "url": "/redditor/index.html"
+  },
+  {
+    "revision": "69f704a90401b4c8afd8",
+    "url": "/redditor/static/css/main.47264f1f.chunk.css"
+  },
+  {
+    "revision": "8509cd61755d5fa6a734",
+    "url": "/redditor/static/js/2.4bbcc0ff.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/redditor/static/js/2.4bbcc0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f704a90401b4c8afd8",
+    "url": "/redditor/static/js/main.afe41497.chunk.js"
+  },
+  {
+    "revision": "7403b5624f1bd8fa4dfe",
+    "url": "/redditor/static/js/runtime-main.d15c973d.js"
+  }
+]);
