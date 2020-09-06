2dab908cb175eb88d85ff590345c0ad6abc350b2
arjun08soni <arjun08soni@gmail.com>
2020-09-06 17:00:38 +0530

    Create gh-pages branch with static content

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import index from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
